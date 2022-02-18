e1510ad1bec2ae0ffbd09db0af820082341bb34e
unknown <rodrigo.domingues.87@hotmail.com>
2022-02-18 10:42:47 -0300

    Mapeando relacionamentos

--- src/main/java/br/com/alura/lojajpa/testes/CadastroDeProduto.java
+++ src/main/java/br/com/alura/lojajpa/testes/CadastroDeProduto.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 
 import javax.persistence.EntityManager;
 
+import br.com.alura.lojajpa.dao.CategoriaDao;
 import br.com.alura.lojajpa.dao.ProdutoDao;
 import br.com.alura.lojajpa.modelo.Categoria;
 import br.com.alura.lojajpa.modelo.Produto;
@@ -12,15 +13,22 @@ import br.com.alura.lojajpa.util.JPAUtil;
 public class CadastroDeProduto {
 
 	public static void main(String[] args) {
-		Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), Categoria.CELULARES );
+		
+		Categoria celulares = new Categoria("CELULARES");	
+		Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), celulares );
 		
 		EntityManager em = JPAUtil.getEntityManager();
 		
-	    ProdutoDao dao = new ProdutoDao(em);
+	    ProdutoDao produtoDao = new ProdutoDao(em);
+	    CategoriaDao categoriaDao = new CategoriaDao(em);
 	    
 	    em.getTransaction().begin();
-	    dao.cadastrar(celular);
+	    
+	    categoriaDao.cadastrar(celulares);
+	    produtoDao.cadastrar(celular);
 	    em.getTransaction().commit();
+	    
+	    
 	    em.close();
 	   
 	        }   
