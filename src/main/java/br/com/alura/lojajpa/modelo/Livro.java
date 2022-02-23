9feb8e0b5e6a92fa3409efda3cb16fd3b2757269
unknown <rodrigo.domingues.87@hotmail.com>
2022-02-23 16:13:36 -0300

    Livro

--- src/main/java/br/com/alura/lojajpa/modelo/Livro.java
+++ src/main/java/br/com/alura/lojajpa/modelo/Livro.java
@@ -1,5 +1,32 @@
 package br.com.alura.lojajpa.modelo;
 
-public class Livro {
+import javax.persistence.Entity;
 
+@Entity
+public class Livro extends Produto {
+	
+	private String autor;
+	private Integer numeroDePaginas;
+	
+	public Livro() {
+	}
+	
+	public Livro(String autor, Integer numeroDePaginas) {
+		this.autor = autor;
+		this.numeroDePaginas = numeroDePaginas;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public Integer getNumeroDePaginas() {
+		return numeroDePaginas;
+	}
+	public void setNumeroDePaginas(Integer numeroDePaginas) {
+		this.numeroDePaginas = numeroDePaginas;
+	}
+	
 }
